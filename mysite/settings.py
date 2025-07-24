0bbda3591d62b9c55177c503c884bb7f3560a66d
Austin Bratton <ausbratton@gmail.com>
2025-07-24 10:18:22 -0500

    Updated static files

--- mysite/settings.py
+++ mysite/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'palettes', 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
